a94093663dbaccd6dfcf7a4e2118db7a90f056af
vidhi <vidhidoshi039@gmail.com>
2022-07-08 16:02:07 +0530

    Last commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-
+    path('postform', views.post_form, name='post_form'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post_del/<int:pk>/', views.post_del, name='post_del'),
+    
 ]
